23674f05c2428f1790e6cf33c4ea69037ea670eb
Rishi <hopesalive1403@gmail.com>
2025-03-21 16:00:31 +0530

    Exploring Validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = mongoose.Schema({
     firstName: {
@@ -21,13 +22,21 @@ const userSchema = mongoose.Schema({
         required: true,
         lowercase: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)) {
+                throw new Error("Invalid email address : " + value)
+            }
+        }
     },
     password: {
         type: String,
         required: true,
-        lowercase: true,
-        trim: true,
-        minLength: 7,
+
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error(value + " is a Weak password, Enter Strong Password...")
+            }
+        }
     },
     age: {
         type: Number,
@@ -44,6 +53,12 @@ const userSchema = mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://png.pngtree.com/png-clipart/20231019/original/pngtree-user-profile-avatar-png-image_13369988.png",
+
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid URL : " + value)
+            }
+        }
     },
     about: {
         type: String,
