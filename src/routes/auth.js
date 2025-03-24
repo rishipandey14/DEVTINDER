1cff022705e912185aab53cf8d4b5ab4353342b5
Rishi <hopesalive1403@gmail.com>
2025-03-24 11:16:11 +0530

    created auth route handler to store authorization related routers like login and signup

---
+++ src/routes/auth.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const authRouter = express.Router();
+const {validateSignupData} = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+
+authRouter.post("/signup", async (req, res) => {
+    try {
+        // validation of data
+        validateSignupData(req);
+
+        const { firstName, lastName, emailId, password } = req.body;
+
+        // encrypt the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // create new instance of the User model
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: hashedPassword,
+        });
+
+        await user.save();
+        res.status(201).send({ message: "User added successfully", user });
+    } catch (err) {
+        res.status(400).send("Error saving the user : " + err.message);
+    }
+});
+
+authRouter.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
+            throw new Error("Invalid credentials");
+        }
+
+        const isPasswordValid = await user.validatePassword(password);
+
+        if(isPasswordValid){
+            // create a JWT token
+            const token = await user.getJWT();
+
+            // Add the token to cookie and send response back to the user
+            res.cookie("token", token, {expires : new Date(Date.now() + 8 * 3600000)});
+            res.send("Login Successfully");
+        }
+        else {
+            throw new Error("Invalid credentials");
+        }
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
+
+module.exports = authRouter;
