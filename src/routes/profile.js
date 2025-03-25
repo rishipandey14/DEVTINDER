d505f81ddc4f56206513d2278ee7bf360349efe1
Rishi <hopesalive1403@gmail.com>
2025-03-25 14:37:41 +0530

    created password change API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const profileRouter = express.Router();
 const {userAuth} = require("../middlewares/auth");
-const {validateEditProfileData} = require("../utils/validation")
+const {validateEditProfileData} = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 
+// view Profile API
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try{
         const user = req.user;
@@ -13,6 +17,7 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
     }
 });
 
+// edit profile API
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     try {
         if(!validateEditProfileData(req)) throw new Error("Invalid edit request");
@@ -30,6 +35,33 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     }
 });
 
+// password change API
+profileRouter.patch("/profile/password/change", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const {existingPassword, newPassword} = req.body;
+
+        const isExistingPasswordValid = await loggedInUser.validatePassword(existingPassword);
+        if(!isExistingPasswordValid) {
+            return res.status(400).json({error : "Incorrect existing password"})
+        }
+
+        // Check if the new password is strong
+        if (!validator.isStrongPassword(newPassword, { minLength: 8, minNumbers: 1, minSymbols: 1, minUppercase: 1, minLowercase: 1 })) {
+            return res.status(400).json({ error: "Weak password. Use at least 8 characters with uppercase, lowercase, number, and special character." });
+        }
+
+        loggedInUser.password = await bcrypt.hash(newPassword, 10);
+        await loggedInUser.save();
+
+        res.json({
+            message : "Password changed successfully",
+        });
+
+    } catch (err) {
+        res.status(500).json({error : err.message});
+    }
+})
 
 
 
