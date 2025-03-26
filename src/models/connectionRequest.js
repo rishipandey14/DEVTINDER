7e8a8efb9071d87c7834dd6dc61ef2b322d8b2fc
Rishi <hopesalive1403@gmail.com>
2025-03-26 14:03:54 +0530

    did indexing and added reference from User

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -4,10 +4,12 @@ const connectionRequestSchema = new mongoose.Schema(
     {
         fromUserId: {
             type: mongoose.Schema.Types.ObjectId,
+            ref: "User",  // reference to the user collection
             required: true,
         },
         toUserId: {
             type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
             required: true,
         },
         status : {
@@ -21,6 +23,8 @@ const connectionRequestSchema = new mongoose.Schema(
     },
     {timestamps: true},
 );
+
+connectionRequestSchema.index({fromUserId : 1, toUserId : 1});
 
 connectionRequestSchema.pre("save", function (next) {
     const connectionRequest = this;
