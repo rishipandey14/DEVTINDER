93508ff9a4bf0111d3e59b4b8495f5230cf82126
Rishi <hopesalive1403@gmail.com>
2025-03-27 09:53:31 +0530

    added pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -60,8 +60,8 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
-
-userRouter.get("/user/feed", userAuth, async (req, res) => {
+// get the feed for the loggedIn User
+userRouter.get("/feed", userAuth, async (req, res) => {
   try {
 
     // user should see all the cards except
@@ -71,6 +71,11 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
     // 3 -> ignored people
 
     const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;  // sanitization of limit
+    const skip = (page-1)*limit;
 
     // find all connection requests (sent + receive)
     const connectionRequest = await ConnectionRequest.find({
@@ -91,9 +96,9 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         {_id : { $nin : Array.from(hideUsersFromFeed)}},
         {_id : { $ne : loggedInUser._id}},
       ]
-    }).select(USER_SAFE_DATA);
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
 
-    res.send(user);
+    res.json({data : user});
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
